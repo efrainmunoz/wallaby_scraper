608880794629de23073900ef38b37be4c026c137
Efrain <efrain@sg3dev.tk>
2018-10-09 18:22:20 +0000

    Ignore JS errors

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,8 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
-config :wallaby, phantomjs_args: "--ssl-protocol=TLSv1"
+config :wallaby, phantomjs_args: "--ssl-protocol=TLSv1",
+  js_errors: false
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
